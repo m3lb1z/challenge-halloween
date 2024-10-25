38741827a39ab4bb349f5b9c3c0f4af425af0025
Elvis M. <u201112959@upc.edu.pe>
2024-10-25 07:25:14 -0500

    chore: Add maven wrapper and initial project structure

---
+++ src/main/java/emrx/monster/ChallengeHalloweenApplication.java
@@ -0,0 +1,13 @@
+package emrx.monster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeHalloweenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeHalloweenApplication.class, args);
+    }
+
+}
